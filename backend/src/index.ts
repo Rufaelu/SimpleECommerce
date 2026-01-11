c14a85f05617ccb77ae4e1955430b3914c336e4d
Rufaelu <niamraf12@gmail.com>
2026-01-11 19:15:28 +0300

    ..

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,14 @@
 import express from "express";
 import cors from "cors";
+import prisma from "./prisma.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get("/api/health", (req, res) => {
-  res.json({ status: "OK" });
+app.get("/api/health", async (_req, res) => {
+  const users = await prisma.user.findMany();
+  res.json({ status: "OK", users });
 });
 
 app.listen(5000, () => {
